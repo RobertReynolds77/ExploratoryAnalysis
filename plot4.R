e307c927272608b6ee5c41d1dd35ec77417ea0f0
RJR <reynoldsr@gmail.com>
2015-12-13 15:03:58 -0800

    updated save

--- plot4.R
+++ plot4.R
@@ -10,16 +10,29 @@ power <- data.table(fread('household_power_consumption.txt', sep=';', header=T, 
   power$Date <- as.Date(strptime(power$Date,"%d/%m/%Y"))
   power <- power[power$Date>=start & power$Date<=end,]                        
 
-#Plot of Sub_metering_1-3 (plot 3)
+#Plot with multiple sub-plots (plot 4)
 png(file="plot4.png",width = 480, height = 480, units = "px")
+
   par(mfrow=c(2,2))
+  plot(1:2880,power$Global_active_power,type="l",xaxt='n',
+     ylab="Global Active Power",cex.lab=0.9,xlab='')
+  axis(1,at=c(0,1440,2880),labels=c('Thu','Fri','Sat'),cex.axis=0.9)
+
+  plot(1:2880,power$Voltage,type="l",xaxt='n',
+     ylab="Voltage",cex.lab=0.9,xlab='datetime')
+  axis(1,at=c(0,1440,2880),labels=c('Thu','Fri','Sat'),cex.axis=0.9)
+
   plot(1:2880,power$Sub_metering_1,type="l",xaxt="n",
       ylab="Energy sub metering",cex.lab=0.9,xlab="")
   lines(1:2880,power$Sub_metering_2,col="red")
   lines(1:2880,power$Sub_metering_3,col="blue")
   axis(1,at=c(0,1440,2880),labels=c("Thu","Fri","Sat"),cex.axis=0.9)
   legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-      lty=1,col=c("black","red","blue"),cex=0.9)
+      lty=1,col=c("black","red","blue"),cex=0.6,bty="n")
+
+  plot(1:2880,power$Global_reactive_power,type="l",xaxt='n',
+     ylab="Global_reactive_power",cex.lab=0.9,xlab='datetime')
+  axis(1,at=c(0,1440,2880),labels=c('Thu','Fri','Sat'),cex.axis=0.9)
 dev.off()
 
 
